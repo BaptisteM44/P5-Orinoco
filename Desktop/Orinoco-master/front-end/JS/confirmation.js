e18fee1ac7f1c453154d18e3d690d6745c3484c9
Morvan <bapmorvan@gmail.com>
2021-07-07 16:37:39 +0200

    last

---
+++ Desktop/Orinoco-master/front-end/JS/confirmation.js
@@ -0,0 +1,29 @@
+//Récupération de l'id de la commande dans le Local storage
+const responseId = localStorage.getItem("responseId");
+
+//récupération du prix total de la commande
+const prixFinal = localStorage.getItem("prixFinal");
+
+//La structure HTML de la page confirmation
+
+const positionElement2 = document.querySelector("#container-commande");
+
+const structureConfirmation = `
+  <h2>Merci pour votre commande</h2>
+  <div class="recapCommande">
+    <p class="txt">Votre commande numéro: <span>${responseId}</span> a bien été prise en compte</p>
+    <p class="txt">Le total de votre commande: <span>${prixFinal} €</span></p>
+  </div>`;
+
+//Injection HTML
+positionElement2.insertAdjacentHTML("afterbegin", structureConfirmation);
+
+//effacer le localStorage sauf le formulaire
+
+function deleteLocalStorage(key) {
+  localStorage.removeItem(key);
+}
+
+deleteLocalStorage("prixFinal");
+deleteLocalStorage("produit");
+deleteLocalStorage("responseId");
